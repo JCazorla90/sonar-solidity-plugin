79cb2b1a9da60fb6842745a1d18264b3b62144a2
Jose Cazorla <40724062+JCazorla90@users.noreply.github.com>
2025-03-18 17:32:49 +0100

    Update SolidityLanguage.java

--- src/main/java/org/sonarsource/solidity/SolidityLanguage.java
+++ src/main/java/org/sonarsource/solidity/SolidityLanguage.java
@@ -1,1 +1,18 @@
+package org.sonarsource.solidity;
 
+import org.sonar.api.resources.AbstractLanguage;
+
+public class SolidityLanguage extends AbstractLanguage {
+
+    public static final String KEY = "solidity";
+    public static final Predicate<InputFile> PREDICATE = inputFile -> inputFile.filename().endsWith(".sol");
+
+    public SolidityLanguage() {
+        super(KEY, "Solidity");
+    }
+
+    @Override
+    public String[] getFileSuffixes() {
+        return new String[] {".sol"};
+    }
+}
